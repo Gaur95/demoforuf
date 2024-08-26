79d71fb97fbac5f37bf1910711e67a8a58a61f6e
Akash Gaur <104058163+Gaur95@users.noreply.github.com>
2024-08-26 17:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "akres" {
     ami = "ami-0e86e20dae9224db8"
     instance_type = "t2.micro"
-    key_name = "ak44"
+    key_name = "nvkey"
     tags = {
       "Name" = "myterraforminst"
     }
@@ -14,7 +14,26 @@ resource "aws_instance" "akres" {
       type = "ssh"
       user = "ubuntu"
       host = self.public_ip
-      private_key = file("/home/akash/Downloads/ak44.pem")
+      private_key = file("/home/gur/Downloads/nvkey.pem")
       }
-      ## TASK------> try with user_date to install apache2 /make a dir 
+
+    provisioner "file" {
+      source = "abc.txt"
+      destination = "/home/ubuntu/abc.txt"
+    }
+    provisioner "remote-exec" {
+        inline = [ "mkdir hey12" ]
+      
+    }
+}
+
+output "my_public_ip" {
+  value = aws_instance.akres.public_ip
+}
+output "key" {
+  value = aws_instance.akres.key_name
+}
+output "pirvateIP" {
+    value = aws_instance.akres.private_ip
+  
 }
